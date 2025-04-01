46ecf25818f418b052595691e65f4655bac1e422
Jose Sancho <jose.sancho89@gmail.com>
2025-04-01 22:12:43 +0000

    0.2

--- src/models.py
+++ src/models.py
@@ -1,15 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer, ForeignKey, PrimaryKeyConstraint, JSON
+from sqlalchemy import String, Boolean, Integer, ForeignKey, Table, Column 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
+
+#table for relationships M <-> M
+favorites = Table('favorites', db.Model.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('favorite_id', Integer, nullable=False),
+    Column('favorite_type', String(20), nullable=False)  # "planet" or "character"
+)
 
 class User(db.Model):
     __tablename__ = 'user'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    favorites: Mapped[list] = mapped_column(JSON, default=list) 
+    #relationships
+    favorites: Mapped[list] = relationship("Favorite", back_populates="user")
 
     def serialize(self):
         return {
@@ -22,7 +30,6 @@ class User(db.Model):
 
 
 class Planet(db.Model):
-    __tablename__ = 'planet'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     uid: Mapped[int] = mapped_column(Integer, unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
@@ -34,7 +41,6 @@ class Planet(db.Model):
     rotation_period: Mapped[int] = mapped_column(Integer)
     terrain: Mapped[str] = mapped_column(String(100))
     url: Mapped[str] = mapped_column(String(200), nullable=False)
-    favorites: Mapped[list] = relationship('Favorites', back_populates='planet')
 
     def serialize(self):
         return {
@@ -51,20 +57,7 @@ class Planet(db.Model):
             "url": self.url,
         }
 
-class Planets(db.Model):
-    __tablename__ = 'planets'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    uid: Mapped[int] = mapped_column(Integer, db.ForeignKey('planet.uid'), nullable=False)
-    url: Mapped[str] = mapped_column(String(200), nullable=False)
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "uid": self.uid,
-            "url": self.url,
-        }
-
-class Person(db.Model):
+class Character(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     uid: Mapped[int] = mapped_column(Integer, unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
@@ -77,7 +70,8 @@ class Person(db.Model):
     homeworld: Mapped[str] = mapped_column(String(200))
     birth_year: Mapped[str] = mapped_column(String(20))
     url: Mapped[str] = mapped_column(String(200), nullable=False)
-    favorites: Mapped[list] = relationship('Favorites', back_populates='person')
+    #relationships
+    homeworld: Mapped["Planet"] = relationship("Planet")
 
     def serialize(self):
         return {
@@ -95,28 +89,19 @@ class Person(db.Model):
             "url": self.url,
         }
 
-class People(db.Model):
-    __tablename__ = 'people'
+class Favorite(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    uid: Mapped[int] = mapped_column(Integer, db.ForeignKey('person.uid'), nullable=False)
-    url: Mapped[str] = mapped_column(String(200), nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'), nullable=False)
+    favorite_id: Mapped[int] = mapped_column(Integer, nullable=False)  # ID of either a planet or character
+    favorite_type: Mapped[str] = mapped_column(String(20), nullable=False)  # "planet" or "character"
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "uid": self.uid,
-            "url": self.url,
-        }
-
-
-class Favorites(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer, db.ForeignKey('user.id'), nullable=False)
-    favorite_uid: Mapped[int] = mapped_column(Integer, nullable=False)  
+    #relationships
+    user: Mapped["User"] = relationship("User", back_populates="favorites")
 
     def serialize(self):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "favorite_uid": self.favorite_uid,
+            "favorite_id": self.favorite_id,
+            "favorite_type": self.favorite_type
         }
